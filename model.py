e846245f3f02ac98c63e51cab32d35980fc0e944
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-05-01 00:16:19 +0530

    optimized params

--- model.py
+++ model.py
@@ -9,36 +9,31 @@ import torch
 import torch.nn as nn
 from x_transformers import ViTransformerWrapper, TransformerWrapper, Encoder, Decoder
 
-# 
-
+# Define the model architecture
 class SceneScript(nn.Module):
-    def __init__(self):
+    def __init__(self, encoder_params, decoder_params):
         super(SceneScript, self).__init__()
+        
+        # Encoder parameters
+        encoder_attn_layers_params = encoder_params.pop('attn_layers')
+        encoder_attn_layers = Encoder(**encoder_attn_layers_params)
+        encoder_params['attn_layers'] = encoder_attn_layers
+
+        # Decoder parameters
+        decoder_attn_layers_params = decoder_params.pop('attn_layers')
+        decoder_attn_layers = Decoder(**decoder_attn_layers_params)
+        decoder_params['attn_layers'] = decoder_attn_layers
+
         # Define the image encoder
-        self.encoder = ViTransformerWrapper(
-            image_size=IMAGE_SIZE,
-            patch_size=PATCH_SIZE,
-            attn_layers=Encoder(
-                dim=DIMENSION,
-                depth=LAYERS,
-                heads=HEADS
-            )
-        )
+        self.encoder = ViTransformerWrapper(**encoder_params)
+
         # Define the caption decoder
-        self.decoder = TransformerWrapper(
-            num_tokens=VOCAB_SIZE,
-            max_seq_len=MAX_SEQ_LEN,
-            attn_layers=Decoder(
-                dim=DIMENSION,
-                depth=LAYERS,
-                heads=HEADS,
-                cross_attend=True
-            )
-        )
+        self.decoder = TransformerWrapper(**decoder_params)
         
     def forward(self, img, caption):
         # Encode the image
         encoded = self.encoder(img, return_embeddings=True)
         # Decode the caption
         output = self.decoder(caption, context=encoded)
+        
         return output
