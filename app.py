8a7c8814e4f842a166834316c3a6b03a6aecb675
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-05-02 03:10:24 +0530

    streamlit app

--- app.py
+++ app.py
@@ -1,25 +1,72 @@
+# Importing dependencies
 import streamlit as st
-import torch
-from PIL import Image
 import requests
 from io import BytesIO
-from scene_script import generate_caption
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from transformers import GPT2TokenizerFast
+from PIL import Image
+import os
+import json
+import argparse
+from model import SceneScript
 
+# Set page title, icon and layout
 st.set_page_config(page_title="Scene Script", page_icon=":memo:", layout="wide")
+
+# Set page title and description
+st.title("Scene Script")
+st.markdown("This is a demo of the Scene Script model. The model generates a caption for a given image.")
+
+# Check if GPU is available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Loading model config
+model_param = '97M'
+with open(f'config/{model_param}.json'.format(model_param)) as f:
+    params = json.load(f)
+
+# Encoder Parameters
+encoder_params = params.pop('encoder')
+# Decoder Parameters
+decoder_params = params.pop('decoder')
 
 # Define the data preprocessing pipeline
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize((encoder_params['image_size'], encoder_params['image_size'])),
     transforms.ToTensor()
 ])
 
 # Load the tokenizer
 tokenizer = GPT2TokenizerFast.from_pretrained('gpt2')
 tokenizer.pad_token_id = tokenizer.eos_token_id
-tokenizer.add_special_tokens({'bos_token': ''})
+tokenizer.add_special_tokens({'bos_token': '<|startoftext|>'})
+
+# Define function for generating caption
+def generate_caption(model, image_path, tokenizer):
+    # Open image and apply transformations
+    image = Image.open(image_path).convert('RGB')
+    image = transform(image).unsqueeze(0).to(device)
+
+    # Generate caption
+    with torch.no_grad():
+        encoded = model.encoder(image, return_embeddings=True)
+        caption_tokens = [tokenizer.bos_token_id]
+        for i in range(decoder_params['max_seq_len']):
+            caption_tensor = torch.tensor(caption_tokens).unsqueeze(0).to(device)
+            output = model.decoder(caption_tensor, context=encoded)
+            last_token_logits = output[0, -1, :]
+            next_token_id = torch.argmax(last_token_logits).item()
+            caption_tokens.append(next_token_id)
+    
+    # Decode caption
+    caption = tokenizer.decode(caption_tokens, skip_special_tokens=True)
+    return caption
 
 # Load the model
-model = torch.load('scene-script.pth', map_location=torch.device('cpu'))
+model = SceneScript(encoder_params, decoder_params).to(device)
+model.load_state_dict(torch.load('weights/scene-script.pth', map_location=device))
 model.eval()
 
 # Create sidebar for file upload or image URL
@@ -42,14 +89,13 @@ else:
             st.sidebar.warning("Invalid URL entered. Please try again.")
 
 # Generate caption when 'Generate Caption' button is clicked
-if st.sidebar.button("Generate Caption"):
+if st.button("Generate Caption"):
     if option == 'Upload Image':
         if uploaded_file is None:
             st.warning("Please upload an image first.")
         else:
-            image_tensor = transform(image).unsqueeze(0)
-            caption = generate_caption(model, image_tensor, tokenizer)
-            st.success("Generated Caption: " + caption)
+            caption = generate_caption(model, uploaded_file, tokenizer)
+            st.write('\nCaption: ' + caption)
     else:
         if image_url == "":
             st.warning("Please enter an image URL first.")
